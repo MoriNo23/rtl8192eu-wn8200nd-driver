cda69f252e643876d1e33a2b33fb9b1fbdcffb63
clnhub <daveholden2010@gmail.com>
2025-10-10 12:03:20 +0200

    Add kernel 5.17(+) support

--- os_dep/linux/rtw_proc.c
+++ os_dep/linux/rtw_proc.c
@@ -232,7 +232,11 @@ const int drv_proc_hdls_num = sizeof(drv_proc_hdls) / sizeof(struct rtw_proc_hdl
 static int rtw_drv_proc_open(struct inode *inode, struct file *file)
 {
 	/* struct net_device *dev = proc_get_parent_data(inode); */
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 17, 0))
+	ssize_t index = (ssize_t)pde_data(inode);
+#else	
 	ssize_t index = (ssize_t)PDE_DATA(inode);
+#endif	
 	const struct rtw_proc_hdl *hdl = drv_proc_hdls + index;
 	void *private = NULL;
 
@@ -258,7 +262,12 @@ static int rtw_drv_proc_open(struct inode *inode, struct file *file)
 
 static ssize_t rtw_drv_proc_write(struct file *file, const char __user *buffer, size_t count, loff_t *pos)
 {
+
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 17, 0))
+	ssize_t index = (ssize_t)pde_data(file_inode(file));
+#else
 	ssize_t index = (ssize_t)PDE_DATA(file_inode(file));
+#endif	
 	const struct rtw_proc_hdl *hdl = drv_proc_hdls + index;
 	ssize_t (*write)(struct file *, const char __user *, size_t, loff_t *, void *) = hdl->write;
 
@@ -5368,7 +5377,11 @@ const int adapter_proc_hdls_num = sizeof(adapter_proc_hdls) / sizeof(struct rtw_
 
 static int rtw_adapter_proc_open(struct inode *inode, struct file *file)
 {
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 17, 0))
+	ssize_t index = (ssize_t)pde_data(inode);
+#else
 	ssize_t index = (ssize_t)PDE_DATA(inode);
+#endif	
 	const struct rtw_proc_hdl *hdl = adapter_proc_hdls + index;
 	void *private = proc_get_parent_data(inode);
 
@@ -5394,7 +5407,11 @@ static int rtw_adapter_proc_open(struct inode *inode, struct file *file)
 
 static ssize_t rtw_adapter_proc_write(struct file *file, const char __user *buffer, size_t count, loff_t *pos)
 {
+if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 17, 0))
+	ssize_t index = (ssize_t)pde_data(file_inode(file));
+#else
 	ssize_t index = (ssize_t)PDE_DATA(file_inode(file));
+#endif	
 	const struct rtw_proc_hdl *hdl = adapter_proc_hdls + index;
 	ssize_t (*write)(struct file *, const char __user *, size_t, loff_t *, void *) = hdl->write;
 
@@ -5557,7 +5574,11 @@ const int odm_proc_hdls_num = sizeof(odm_proc_hdls) / sizeof(struct rtw_proc_hdl
 
 static int rtw_odm_proc_open(struct inode *inode, struct file *file)
 {
+if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 17, 0))
+	ssize_t index = (ssize_t)pde_data(inode);
+#else
 	ssize_t index = (ssize_t)PDE_DATA(inode);
+#endif	
 	const struct rtw_proc_hdl *hdl = odm_proc_hdls + index;
 	void *private = proc_get_parent_data(inode);
 
@@ -5583,7 +5604,11 @@ static int rtw_odm_proc_open(struct inode *inode, struct file *file)
 
 static ssize_t rtw_odm_proc_write(struct file *file, const char __user *buffer, size_t count, loff_t *pos)
 {
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 17, 0))
+	ssize_t index = (ssize_t)pde_data(file_inode(file));
+#else
 	ssize_t index = (ssize_t)PDE_DATA(file_inode(file));
+#endif	
 	const struct rtw_proc_hdl *hdl = odm_proc_hdls + index;
 	ssize_t (*write)(struct file *, const char __user *, size_t, loff_t *, void *) = hdl->write;
 
@@ -5722,7 +5747,11 @@ const int mcc_proc_hdls_num = sizeof(mcc_proc_hdls) / sizeof(struct rtw_proc_hdl
 
 static int rtw_mcc_proc_open(struct inode *inode, struct file *file)
 {
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 17, 0))
+	ssize_t index = (ssize_t)pde_data(inode);
+#else
 	ssize_t index = (ssize_t)PDE_DATA(inode);
+#endif	
 	const struct rtw_proc_hdl *hdl = mcc_proc_hdls + index;
 	void *private = proc_get_parent_data(inode);
 
@@ -5748,7 +5777,11 @@ static int rtw_mcc_proc_open(struct inode *inode, struct file *file)
 
 static ssize_t rtw_mcc_proc_write(struct file *file, const char __user *buffer, size_t count, loff_t *pos)
 {
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 17, 0))
+	ssize_t index = (ssize_t)pde_data(file_inode(file));
+#else
 	ssize_t index = (ssize_t)PDE_DATA(file_inode(file));
+#endif	
 	const struct rtw_proc_hdl *hdl = mcc_proc_hdls + index;
 	ssize_t (*write)(struct file *, const char __user *, size_t, loff_t *, void *) = hdl->write;
 
