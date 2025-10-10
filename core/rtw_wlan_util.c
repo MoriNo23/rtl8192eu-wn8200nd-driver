137f11214fa04e0f301b17969351eaed69c336ca
clnhub <daveholden2010@gmail.com>
2025-10-10 18:35:11 +0200

    Improve sanitizer behavior

--- core/rtw_wlan_util.c
+++ core/rtw_wlan_util.c
@@ -1782,6 +1782,7 @@ void HT_get_ss_from_mcs_set(u8 *mcs_set, u8 *Rx_ss)
 	*Rx_ss = r_ss;
 }
 
+__attribute__((no_sanitize("undefined")))
 void HT_caps_handler_infra_ap(_adapter *padapter, PNDIS_802_11_VARIABLE_IEs pIE)
 {
 	unsigned int	i;
